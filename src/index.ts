d7cbf49215a322d386cdce130c44fbf8ef34871c
EsperLion <germankushchlion31@gmail.com>
2024-10-30 20:05:32 +0200

    base setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // import type { Core } from '@strapi/strapi';
 
+import { Core } from "@strapi/strapi";
+import { contactsSeed } from "../database/seeds/contact_seed";
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +19,11 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    const result = await strapi.documents("api::contact.contact").findMany();
+    console.log({ result });
+    if (result.length === 0) {
+      await contactsSeed(strapi);
+    }
+  },
 };
